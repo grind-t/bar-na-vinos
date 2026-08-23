15848a090aa750ed161a53b05c4537f6eb5d5c5e
grind-t <grindt.gd@gmail.com>
2026-08-23 03:07:46 +0300

    upd

--- src/export-commodities.ts
+++ src/export-commodities.ts
@@ -4,30 +4,21 @@ import { join } from "node:path";
 
 import type { Commodity } from "@grind-t/loyalsuns";
 import sharp from "sharp";
+import * as XLSX from "xlsx";
 import { $ } from "zx";
 
 const YC_BUCKET = "bar-na-vinos-public";
+const EXPORTS_DIR = join(import.meta.dirname, "exports");
 
 const commodities = await $`loyalsuns commodities --size 50 2029385561100861442`
   .json()
   .then((v: Commodity[]) => v.filter(isPublicCommodity));
 
 await uploadImages(commodities);
-
-const result = commodities.map((v) => ({
-  name: v.commodityName,
-  image: uploadedImageUrl(v),
-  hot: !!v.coldOrHot,
-}));
-
-console.log(JSON.stringify(result, null, 2));
+await exportForYandexMap(commodities);
 
 function isPublicCommodity({ status, categoriesName }: Commodity) {
   return !!status && !["test", "water"].includes(categoriesName);
-}
-
-function uploadedImageUrl({ commodityCode }: Commodity) {
-  return `https://storage.yandexcloud.net/${YC_BUCKET}/${commodityCode}.png`;
 }
 
 async function renderImage(commodity: Commodity, tmpDir: string) {
@@ -56,4 +47,41 @@ async function uploadImages(commodities: Commodity[]) {
   await Promise.all(commodities.map((commodity) => renderImage(commodity, tmpDir)));
   await $`yc storage s3 cp ${tmpDir} s3://${YC_BUCKET}/ --recursive`;
   await rm(tmpDir, { recursive: true });
+}
+
+async function exportForYandexMap(commodities: Commodity[]) {
+  const header = [
+    "Категория",
+    "Название",
+    "Идентификатор",
+    "Описание",
+    "Короткое описание",
+    "Цена",
+    "Фото",
+    "Популярный товар",
+    "В наличии",
+    "Количество",
+    "Единицы измерения",
+    "Ссылка",
+  ];
+
+  const rows = commodities.map((v) => [
+    v.categoriesName,
+    v.commodityName,
+    "",
+    "",
+    "",
+    Number(v.commodityPrice),
+    `https://storage.yandexcloud.net/${YC_BUCKET}/${v.commodityCode}.png`,
+    "",
+    "Да",
+    v.coldOrHot ? 250 : 350,
+    "миллилитр",
+    "",
+  ]);
+
+  const sheet = XLSX.utils.aoa_to_sheet([header, ...rows]);
+  const book = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(book, sheet, "Прайс-лист");
+  XLSX.writeFile(book, join(EXPORTS_DIR, "yandex-price-list.xls"), { bookType: "xls" });
 }
